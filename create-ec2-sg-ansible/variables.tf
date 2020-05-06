18cd613b101e3cde51d99ad595143be8de16a325
joshiPriya <43839921+joshiPriya@users.noreply.github.com>
2020-05-06 17:03:59 +0530

    Update variables.tf

--- create-ec2-sg-ansible/variables.tf
+++ create-ec2-sg-ansible/variables.tf
@@ -1,12 +1,4 @@
-variable "access_key" {
-    type = "string"
-    default = "AKIAQR3GCLCAVR4JYEEZ"
-}
 
-variable "secret_key" {
-    type = "string"
-    default = "FvZI7dU991alAjp68hJJ4ehtQSi+lFtmhLJDxMmE"
-}
 
 
 variable "region" {
